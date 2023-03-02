1d8c42329c9a7c0f02e1eeed8e938ec61740f308
KleoVor <t9671253993@gmail.com>
2023-03-02 21:50:38 +0300

    fixed errors1

--- src/test/java/ru/netology/qa55/RadioTest.java
+++ src/test/java/ru/netology/qa55/RadioTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+
 public class RadioTest {
 
     @ParameterizedTest // зададим максимальое кол-во станций через конструктор
